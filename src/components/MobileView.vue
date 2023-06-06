4bc1a09c71918e7086b0417cbb456d2bb22b3eb0
afuentes <alex@glyfo.com>
2023-06-06 01:33:37 +0000

    update

---
+++ src/components/MobileView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+function handleDiscoverNow(evt){
+  console.warn('handleDiscoverNow', evt +  isIOSSafari)
+}
+
+</script>
+<template>
+        <a
+          class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+          @click="handleDiscoverNow(evt)"
+        >
+          Discover Now
+        </a>
+</template>
