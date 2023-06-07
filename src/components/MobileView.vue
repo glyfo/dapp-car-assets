d1b6a9874c051b33ad842ba268a7455e519ef7ee
afuentes <alex@glyfo.com>
2023-06-07 01:20:42 +0000

    update

--- src/components/MobileView.vue
+++ src/components/MobileView.vue
@@ -4,6 +4,17 @@ function handleDiscoverNow(evt){
   console.warn('handleDiscoverNow', evt )
 }
 
+function handlerConnection(){
+  import { JsonRpcProvider, localnetConnection } from '@mysten/sui.js';
+// connect to local RPC server
+const provider = new JsonRpcProvider(localnetConnection);
+// get tokens from the local faucet server
+await provider.requestSuiFromFaucet(
+  '0xcc2bd176a478baea9a0de7a24cd927661cc6e860d5bacecb9a138ef20dbab231',
+);
+
+}
+
 </script>
 <template>
         <a
