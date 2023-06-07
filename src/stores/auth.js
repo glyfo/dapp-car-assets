329e4d4a22c46fa44cd1c2596a506ccc8ba66240
afuentes <alex@glyfo.com>
2023-06-07 03:08:18 +0000

    update

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,13 +1,25 @@
 import {defineStore} from "pinia";
+import { JsonRpcProvider, localnetConnection } from '@mysten/sui.js';
 
+// connect to local RPC server
+const provider = new JsonRpcProvider(localnetConnection);
+
+const objects = await provider.getOwnedObjects({
+    owner: '0xcc2bd176a478baea9a0de7a24cd927661cc6e860d5bacecb9a138ef20dbab231',
+  });
+
+  const txn = await provider.getObject({
+    id: '0xcc2bd176a478baea9a0de7a24cd927661cc6e860d5bacecb9a138ef20dbab231',
+    // fetch the object content field
+    options: { showContent: true },
+  });
+  
 export const KeyStore = defineStore({
     id: 'keystore',
     state: () => ({
-        pbk     : null,
-        prk     : null,
+        account : {public : null , private: null},
         metadata: []
     }),
-
     actions:{
     }
 });
